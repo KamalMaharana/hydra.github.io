52f95a71d3d08097479211c10a13275117282614
KamalMaharana <40832769+KamalMaharana@users.noreply.github.com>
2021-07-27 22:33:46 +0530

    else removed from check.js

--- js/check.js
+++ js/check.js
@@ -2,6 +2,4 @@ var user = sessionStorage.getItem("user")
 console.log(typeof(user))
 if(user == "null") {
     window.location.href = "index.html"
-} else {
-    window.location.href = "afterLogin.html"
 }
