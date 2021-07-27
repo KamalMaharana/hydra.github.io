a58411e11ddf29bc54fe9842fbdc0acab40b19a6
KamalMaharana <40832769+KamalMaharana@users.noreply.github.com>
2021-07-27 22:27:08 +0530

    ELSE statement added in Check.js file

--- js/check.js
+++ js/check.js
@@ -2,4 +2,6 @@ var user = sessionStorage.getItem("user")
 console.log(typeof(user))
 if(user == "null") {
     window.location.href = "index.html"
+} else {
+    window.location.href = "afterLogin.html"
 }
